504589b28258d83fda08778bb5ec07f4177f2d64
Jacob Samro <admin@jsam.in>
2016-08-28 23:12:00 +0530

    CROS Fixed

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var companies; 
 
 fs.readFile('nasdaq-company-list.csv', (err, data) => {
