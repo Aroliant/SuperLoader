9d994802e914f2da998dc9f6f8d07cdc2456c996
Jacob Samro <admin@jsam.in>
2016-08-28 23:44:21 +0530

    period error fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+    io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 3000);
 
